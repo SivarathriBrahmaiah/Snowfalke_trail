c933d89ef692eba77084f967e11a34ec99d6090e
SivarathriBrahmaiah <108463337+SivarathriBrahmaiah@users.noreply.github.com>
2024-02-13 16:28:30 +0530

    Update snow.py

--- snow.py
+++ snow.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('Welcome to streamlit world')
 streamlit.header('First lesson')
-streamli.text('Today logged in')
-streamli.text('Eager to create app')
-streamli.text('It is soo interesting....')
+streamlit.text('Today logged in')
+streamlit.text('Eager to create app')
+streamlit.text('It is soo interesting....')
 
 
