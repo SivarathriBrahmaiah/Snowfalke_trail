8e23eca2c51b73b2583e7df3bd7d9a6af2f980aa
SivarathriBrahmaiah <108463337+SivarathriBrahmaiah@users.noreply.github.com>
2024-02-13 16:30:16 +0530

    Update snow.py
    Added extra data

--- snow.py
+++ snow.py
@@ -4,5 +4,9 @@ streamlit.header('First lesson')
 streamlit.text('Today logged in')
 streamlit.text('Eager to create app')
 streamlit.text('It is soo interesting....')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 
