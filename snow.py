ae14392b1edd75b9c1e12497324ece2eaf851c6e
SivarathriBrahmaiah <108463337+SivarathriBrahmaiah@users.noreply.github.com>
2024-02-13 16:39:01 +0530

    Update snow.py

--- snow.py
+++ snow.py
@@ -10,8 +10,9 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas as pd
-list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(list)
-streamlit.multiselect("Pick some fruits:", list(list.index))
+list_1 = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Pick some fruits:", list(list_1.index))
+streamlit.dataframe(list_1)
+
 
 
