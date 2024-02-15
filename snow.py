b64ed147b42ac1b6883a76333385bb06fc0d12f6
SivarathriBrahmaiah <108463337+SivarathriBrahmaiah@users.noreply.github.com>
2024-02-15 16:02:37 +0530

    Update snow.py

--- snow.py
+++ snow.py
@@ -18,7 +18,7 @@ streamlit.dataframe(calories_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 
