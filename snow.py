864b57d8e1d2dba5d292fc871101d73c1e78e2fa
SivarathriBrahmaiah <108463337+SivarathriBrahmaiah@users.noreply.github.com>
2024-02-19 09:59:23 +0530

    Update snow.py

--- snow.py
+++ snow.py
@@ -21,6 +21,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+import snowflake.connector
 
 
 
